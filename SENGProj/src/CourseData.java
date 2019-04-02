9c5369c7b40d91af5014c845aad156fbb16aaa92
scubid <47365906+scubid@users.noreply.github.com>
2019-04-02 16:54:27 -0600

    added comments and documentation

--- SENGProj/src/CourseData.java
+++ SENGProj/src/CourseData.java
@@ -1,5 +1,11 @@
 import java.util.ArrayList;
 
+/**
+* CourseData class extends Course for user input from AddCourse form
+* @author	Angeli Manipon
+* @version 	1.0
+*/
+
 public class CourseData extends Course {
 	
 	public CourseData(String name, int level, int id, Program program, String instructor, String prerequisite, String daysOffered,
@@ -10,12 +16,24 @@ public class CourseData extends Course {
 
 	public static ArrayList<Course> courseL = new ArrayList<Course>();
 	
+	/**
+	* addCourse function to add a course to the course list
+	* @param	course - course to be added to list
+	* @return	N/A
+	*/
 	public static void addCourse(Course course)
 	{
 		courseL.add(new Course(course));
 		arrange(courseL, 0, courseL.size()-1);
 	}
-
+	
+	/**
+	* arrange function to arrange course list
+	* @param	ArrayList<Course> - course to be added to list
+	* @param	l 
+	* @param	h
+	* @return	N/A
+	*/
 	public static void arrange(ArrayList<Course> c, int l, int h) {
 		if(c.size() <= 1)
 			return;
@@ -29,7 +47,13 @@ public class CourseData extends Course {
 			}
 		}
 	}
-	
+	/**
+	* part function to arrange course list
+	* @param	ArrayList<Course> - course to be added to list
+	* @param	l 
+	* @param	h
+	* @return	N/A
+	*/
 	private static int part(ArrayList<Course> c, int l, int h) {
 		int i = l - 1;
 		Course piv = c.get(h);
@@ -44,7 +68,14 @@ public class CourseData extends Course {
 		swap(c, i + 1, h);
 		return i + 1;
 	}
-	
+
+	/**
+	* swap function to help arrange course list
+	* @param	ArrayList<Course> - course to be added to list
+	* @param	l 
+	* @param	h
+	* @return	N/A
+	*/
 	private static void swap(ArrayList<Course> c, int i, int j) {
 		Course temp = c.get(i);
 		c.set(i, c.get(j));
