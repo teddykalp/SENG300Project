57071b7537acb398219bef582add4b9e724be5fe
golddragon276 <43428502+golddragon276@users.noreply.github.com>
2019-04-07 20:31:00 -0600

    Add files via upload

---
+++ SENGProj/src/ImageLibrary.java
@@ -0,0 +1,35 @@
+import java.awt.image.*;
+import javax.imageio.*;
+import java.io.*;
+import javax.swing.ImageIcon;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+
+
+/**
+ * Write a description of class ImageLibrary here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ImageLibrary
+{
+      
+        
+        
+        //Forbackgrounds
+         ImageIcon titlepage = new ImageIcon("ImgLib/Titlepage.png");
+         
+         
+         
+         //setArrays 
+         
+         
+         
+    }
+    
+   
+
