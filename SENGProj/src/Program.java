42a84c2702b0f06f80394292c032b0bd4f0481c6
gabyrgonz <36803423+gabyrgonz@users.noreply.github.com>
2019-04-12 15:53:55 -0600

    added comments

--- SENGProj/src/Program.java
+++ SENGProj/src/Program.java
@@ -27,7 +27,6 @@ public class Program {
 	* @param	abbr		course abbreviation
 	* @return 	N/A
 	*/
-	
 	public Program(String name, String description, String department, String level , String type, float gpa, String abbr){
 		this.setName(name);
 		this.setDescription(description);
@@ -38,66 +37,82 @@ public class Program {
 		this.setAbbr(abbr);
 		}
 
+	// get program name
 	public String getName() {
 		return name;
 	}
-
+	
+	// set program name
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	
+	// get program description
 	public String getDescription() {
 		return description;
 	}
-
+	
+	// set program description
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
+	
+	// get department
 	public String getDepartment() {
 		return department;
 	}
-
+	
+	// set department
 	public void setDepartment(String department) {
 		this.department = department;
 	}
-
+	
+	// get program type
 	public String getType() {
 		return type;
 	}
-
+	
+	// set program type
 	public void setType(String type) {
 		this.type = type;
 	}
 
+	// get program gpa
 	public float getGpa() {
 		return gpa;
 	}
-
+	
+	// set program gpa
 	public void setGpa(float gpa) {
 		this.gpa = gpa;
 	}
-
+	
+	// get program level
 	public String getLevel() {
 		return level;
 	}
-
+	
+	// set program level
 	public void setLevel(String level) {
 		this.level = level;
 	}
-
+	
+	// get required array
 	public String[] getRequired() {
 		return required;
 	}
-
+	
+	// set required array
 	public void setRequired(String[] required) {
 		this.required = required;
 	}
 
+	// get abbreviation
 	public String getAbbr() {
 		return abbr;
 	}
-
+	
+	// set abbreviation 
 	public void setAbbr(String abbr) {
 		this.abbr = abbr;
 	}
